09a439d34418775146d3e63b03e7c23331724028
C--B006 <Vuthanhhai>
2022-03-20 13:50:26 +0700

    da lam

--- hello_world1.c
+++ hello_world1.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 int main() {
     printf("hello word\n");
-    printf("chao ban!\t");
-    printf("my friend!");
-    printf("  *\n");
-    printf(" ***\n");
-    printf("*****");
+          printf("chao ban!\t");
+                printf("my friend!");
+                      printf("  *\n");
+                            printf(" ***\n");
+                      printf("*****");
     printf("*****\n")
         printf("*****\n")
         printf("*****\n")
