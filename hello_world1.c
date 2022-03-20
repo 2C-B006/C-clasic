b709c5ab4fe9d10b46fe9e128e4dd89356b6ecc1
C--B006 <Vuthanhhai>
2022-03-20 13:25:12 +0700

    C--B006

---
+++ hello_world1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    printf("hello word\n");
+    printf("chao ban!\t");
+    printf("my friend!");
+    printf("  *\n");
+    printf(" ***\n");
+    printf("*****");
+    printf("*****\n")
+        printf("*****\n")
+        printf("*****\n")
+}
+
